441509823045d42fa31be965061c36357a8e5888
Ivan Daniluk <ivan.daniluk@gmail.com>
2012-05-18 14:09:41 +0300

    Renamed main.qml to deal with the stupid HFS+ case-insensitivity

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     viewer->rootContext()->setContextProperty("codeData", &code);
     viewer->rootContext()->setContextProperty("applicationData", &data);
     viewer->setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
-    viewer->setMainQmlFile(QLatin1String("qml/hackertyper/main.qml"));
+    viewer->setMainQmlFile(QLatin1String("qml/hackertyper/view.qml"));
     viewer->showFullScreen();
 
     return app->exec();
